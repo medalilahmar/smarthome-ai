0e3cb9838b2f6828f2427906dda1fb140c09f47b
medalilahmar <mohamedali.lahmar@esprit.tn>
2025-06-16 19:17:08 +0100

    backend first step

---
+++ smarthome-backend/src/main/java/com/example/smarthomebackend/SmarthomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarthomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarthomeBackendApplication.class, args);
+    }
+
+}
---
+++ smarthome-backend/src/test/java/com/example/smarthomebackend/SmarthomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarthomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
